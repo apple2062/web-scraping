24c3bea79189496c32be8eef5a159c4fac54e701
Yam Ju <apple2062@naver.com>
2020-09-22 10:54:32 +0900

    indeed job scaraping

--- indeed.py
+++ indeed.py
@@ -15,7 +15,7 @@ LIMIT =  50
 URL = "https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python"
 
 #제일 마지막 페이지 번호 수 알아내기
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text,'html.parser')
     pagination = soup.find("div",{"class":"pagination"})  # 맨 하단 페이지 버튼 번호
@@ -41,18 +41,22 @@ def extract_job(html):
             company = (str(company.string)) 
         location = html.find("div",{"class":"recJobLoc"})["data-rc-loc"]
         job_id = html["data-jk"] #esults = soup.find_all("div",{"class":"jobsearch-SerpJobCard"}) < 이놈들마다 id를 불러오
+        return {'title':title,'company':company[1:],'location':location,'link':f"https://kr.indeed.com/viewjob?jk={job_id}"}
+
     
-        return {'title':title,'company':company[1:],'location':location,'link':f}
-
-
 #indeed page를 입력받아서 그 만큼의 Request를 만들기 위한 함수
-def extract_indeed_jobs(last_pages):
+def extract_jobs(last_pages):
     jobs = []
-    #for page in range(last_pages):
-    result = requests.get(f"{URL}&start={0*LIMIT}") #https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={LIMIT}&start={)*LIMIT}
-    soup = BeautifulSoup(result.text,'html.parser')
-    results = soup.find_all("div",{"class":"jobsearch-SerpJobCard"})
-    for result in results:
-        job = extract_job(result)
-        jobs.append(job)
+    for page in range(last_pages):
+        result = requests.get(f"{URL}&start={0*LIMIT}") #https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={LIMIT}&start={)*LIMIT}
+        soup = BeautifulSoup(result.text,'html.parser')
+        results = soup.find_all("div",{"class":"jobsearch-SerpJobCard"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
     return jobs 
+
+def get_jobs():
+    last_page = get_last_page()
+    jobs =extract_jobs(last_page)
+    return jobs
